4c229c4e4e2941b2ebc16e444e5893bff38f3dd2
Patrick Delva <delvapatrick25@gmail.com>
2026-04-09 12:08:00 -0400

    Add GitHub Actions CI pipeline for Dashboard

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ app.use((req, res) => {
 });
 
 // Error Handler
-app.use((err, req, res, next) => {
+app.use((err, req, res, _next) => {
   console.error('Error:', err);
   res.status(err.status || 500).json({
     error: 'Server Error',
